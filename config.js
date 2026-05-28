0cfac7fd2272566cc3ba19bd6542256112560667
Loel Joseph Hofileña <loeljoseph.hofilena@students.isatu.edu.ph>
2026-05-28 09:35:36 +0800

    push

---
+++ config.js
@@ -0,0 +1,10 @@
+// Rename this to config.js and add your keys
+
+export const CONFIG = {
+    
+GEMINI_KEY: "AIzaSyAghGLcPrW_NTk3NyGE1Y3anfG-LuoKopM",
+    
+SUPABASE_URL: "https://pgofejmyhatsjvkscctf.supabase.co",
+    
+SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnb2Zlam15aGF0c2p2a3NjY3RmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMzczNjcsImV4cCI6MjA4ODgxMzM2N30.8e9OCAcShW6bcybpqqR8D0Ie4m0uUn2kFPv5h-TOEnQ"
+};
